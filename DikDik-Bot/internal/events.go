2d15590f322059870232cad1f6f4622b03c4bcac
JessieLoser <jwasner777@gmail.com>
2020-08-20 18:21:18 -0400

    Update 22
    Reorganized project
    Hard coded commands
    Bot now reads jokes and facts from pipe delimited txt files

--- DikDik-Bot/internal/events.go
+++ DikDik-Bot/internal/events.go
@@ -1,4 +1,4 @@
-package main
+package internal
 
 import (
 	"fmt"
@@ -7,19 +7,24 @@ import (
 	"time"
 )
 
-func OnMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
+func (bot *Bot) onMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
 	if msg.Author.Bot {
 		return
 	}
 	//confirm prefix is correct
 	if len(msg.GuildID) == 0 {
 		return
-	} else if !strings.HasPrefix(msg.Content, config.Prefix) && !strings.HasPrefix(msg.Content, "`"+config.Prefix) && !strings.HasPrefix(msg.Content, "``"+config.Prefix) {
+	} else if !strings.HasPrefix(msg.Content, bot.config.Prefix) &&
+		!strings.HasPrefix(msg.Content, "`"+bot.config.Prefix) &&
+		!strings.HasPrefix(msg.Content, "``"+bot.config.Prefix) {
 		//confirms user is commenting in the correct channel
-		if msg.ChannelID == cm[msg.Author.Username] {
+		if msg.ChannelID == bot.allVars.cm[msg.Author.Username] {
 			//confirms say is active for user and posts all messages to other channel
-			if _, exists := m[msg.Author.Username]; exists {
-				OnText(s, msg)
+			if err, exists := bot.allVars.m[msg.Author.Username]; exists {
+				if err != "" {
+					fmt.Println(err)
+				}
+				bot.onText(s, msg)
 			} else {
 				return
 			}
@@ -31,7 +36,7 @@ func OnMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
 		args := strings.Split(""+msg.Content, " ")
 		//used specifically on say to clean up text
 		if len(args[:]) > 1 {
-			if args[0] == config.Prefix+"+say" {
+			if args[0] == bot.config.Prefix+"+say" {
 				if len(args[:]) > 2 {
 					args[2] = strings.Join(args[2:], " ")
 					//makes sure all spaces are trimmed from front and back
@@ -53,7 +58,10 @@ func OnMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
 					if err != nil {
 						fmt.Println(args[1], err)
 						// Could not find channel.
-						s.ChannelMessageSend(msg.ChannelID, "Invalid Channel. Use /help to see commands")
+						_, err := s.ChannelMessageSend(msg.ChannelID, "Invalid Channel. Use /help to see commands")
+						if err != nil {
+							fmt.Println(err)
+						}
 						return
 					}
 				} else
@@ -62,7 +70,10 @@ func OnMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
 					if err != nil {
 						fmt.Println(args[1], err)
 						// Could not find channel.
-						s.ChannelMessageSend(msg.ChannelID, "Invalid Channel. Use /help to see commands")
+						_, err := s.ChannelMessageSend(msg.ChannelID, "Invalid Channel. Use /help to see commands")
+						if err != nil {
+							fmt.Println(err)
+						}
 						return
 					}
 				}
@@ -76,30 +87,30 @@ func OnMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
 		args[0] = strings.ToLower(args[0])
 		switch args[0] {
 		case "+say":
-			OnSet(s, msg, args[:])
+			bot.onSet(s, msg, args[:])
 			break
 		case "-say":
-			OnUnset(s, msg, args[:])
+			bot.onUnset(s, msg, args[:])
 		case "jokethere":
-			OnJokeThere(s, msg, args[:])
+			bot.onJokeThere(s, msg, args[:])
 			break
 		case "factsthere":
-			OnFactsThere(s, msg, args[:])
+			bot.onFactsThere(s, msg, args[:])
 			break
 		case "factshere":
-			OnFactsHere(s, msg)
+			bot.onFactsHere(s, msg)
 			break
 		case "jokehere":
-			OnJokeHere(s, msg)
+			bot.onJokeHere(s, msg)
 			break
 		case "help":
-			OnHelp(s, msg)
+			bot.onHelp(s, msg)
 			break
 		case "delete":
-			OnDelete(s, msg)
+			bot.onDelete(s, msg)
 			break
 		case "status":
-			OnStatus(s, msg)
+			bot.onStatus(s, msg)
 			break
 		default:
 			break
@@ -107,17 +118,24 @@ func OnMessage(s *discordgo.Session, msg *discordgo.MessageCreate) {
 	}
 }
 
-func OnReady(s *discordgo.Session, ready *discordgo.Ready) {
-	//creates say active map
-	m = make(map[string]string)
-	//create prior message map
-	dm = make(map[string]string)
-	//create current channel map
-	cm = make(map[string]string)
-	//create timestamp
-	tm = make(map[string]time.Time)
+func (bot Bot) onEdit(s *discordgo.Session, editmsg *discordgo.MessageUpdate) {
+	if editmsg.EditedTimestamp != "" {
+		if _, exists := bot.allVars.m[editmsg.Author.Username]; exists {
 
-	//confirms bot is ready
-	testing := fmt.Sprintf("ready your %s\n", ready.User.Username)
-	fmt.Printf(testing)
+			//edit message in other channel
+			_, err := s.ChannelMessageEdit(bot.allVars.m[editmsg.Author.Username], bot.allVars.dm[bot.allVars.m[editmsg.Author.Username]], editmsg.Content)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			//set timestamp to last message sent
+			bot.allVars.tm[editmsg.Author.Username] = time.Now()
+			//confirms message has been edited in other channel
+			_, errd := s.ChannelMessageSend(editmsg.Author.Username, "The message has been edited")
+			if errd != nil {
+				fmt.Println(errd)
+			}
+
+		}
+	}
 }
