e32461332bbe5c0e4e510c180b2317a9ca9c4596
Dylan Hackworth <dhpf@pm.me>
2020-09-10 12:07:59 -0500

    Added env var for custom config path
    Added env var for custom config path

--- internal/config.go
+++ internal/config.go
@@ -3,6 +3,7 @@ package internal
 import (
 	util "github.com/Floor-Gang/utilpkg/config"
 	"log"
+	"os"
 )
 
 type DikDikConfig struct {
@@ -12,14 +13,20 @@ type DikDikConfig struct {
 	CSVPathFacts string `yaml:"csv_path_facts"`
 }
 
-const ConfigPath = "dikdik-config.yml"
+const defaultPath = "dikdik-config.yml"
 
 func GetConfig() (config DikDikConfig) {
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if len(configPath) == 0 {
+		configPath = defaultPath
+	}
+
 	config = DikDikConfig{
 		Prefix: "/",
 	}
 
-	err := util.GetConfig(ConfigPath, &config)
+	err := util.GetConfig(configPath, &config)
 	if err != nil {
 		log.Fatalln(err)
 	}
