1908cd058fd20541efca4ce66f94ac1786078f20
Dylan Hackworth <dylhack@outlook.com>
2021-05-20 17:19:50 -0500

    added snakeyaml engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.newcircuit"
-version = "1.0.0"
+version = "2.0.0"
 
 repositories {
     mavenCentral()
@@ -17,6 +17,7 @@ dependencies {
     implementation("com.charleskorn.kaml:kaml:0.33.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.snakeyaml:snakeyaml-engine:2.3")
 }
 
 tasks.jar {
