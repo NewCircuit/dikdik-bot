14bc543837b002313f001a56d3769f8807d40671
Dylan Hackworth <dylhack@outlook.com>
2021-06-03 22:12:53 -0500

    removed command test server

--- src/main/kotlin/io/newcircuit/dikdik/Bot.kt
+++ src/main/kotlin/io/newcircuit/dikdik/Bot.kt
@@ -10,9 +10,7 @@ import io.newcircuit.dikdik.models.Command
 import io.newcircuit.dikdik.models.Question
 import org.javacord.api.DiscordApi
 import org.javacord.api.DiscordApiBuilder
-import org.javacord.api.interaction.ApplicationCommand
 import org.javacord.api.interaction.ApplicationCommandBuilder
-import java.util.concurrent.CompletableFuture
 
 
 class Bot(val config: Config) {
@@ -39,7 +37,6 @@ class Bot(val config: Config) {
     }
 
     private fun registerCommands() {
-        val testServer = client.getServerById(718433475828645928).get()
         val cmds = arrayListOf(
             Joke(this),
             Fact(this),
@@ -50,16 +47,6 @@ class Bot(val config: Config) {
             CloseVote(this),
         )
 
-        val globalCmds = client.globalApplicationCommands.join()
-        val hasCmd = fun(name: String): Boolean {
-            for (cmd in globalCmds) {
-                if (cmd.name == name) {
-                    return true
-                }
-            }
-            return false
-        }
-
         for (command in cmds) {
             val builder = ApplicationCommandBuilder()
                 .setName(command.name)
@@ -69,9 +56,6 @@ class Bot(val config: Config) {
                 builder.addOption(option.build())
             }
             println("Registering: ${command.name}")
-            if (!hasCmd(command.name)) {
-                builder.createForServer(testServer).join()
-            }
             builder.createGlobal(client).join()
             this.commands.add(command)
         }
